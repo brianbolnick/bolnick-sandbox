63eed2240ccb12db2d2f1fd4fbab917441d3c66f
“Brian <brianbolnick@gmail.com>
2018-04-22 00:14:57 -0600

    add pages dir

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default HomePage;
---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFoundPage extends Component {
+  render() {
+    return (
+      <div className="not-found-page">
+        Sorry, that page cannot be found. (404)
+      </div>
+    );
+  }
+}
+
+export default NotFoundPage;
